41807ad2ec0d96734bd2a9d40b1f29fab741cc25
Paola Zanella <Paola Zanella@192.168.0.105>
2022-05-27 10:58:50 -0300

    cm-add server-tipo de imovel

---
+++ src/main/java/br/com/desafio2/tipoimovelservice/TipoImovelServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafio2.tipoimovelservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TipoImovelServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TipoImovelServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/desafio2/tipoimovelservice/TipoImovelServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafio2.tipoimovelservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TipoImovelServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
