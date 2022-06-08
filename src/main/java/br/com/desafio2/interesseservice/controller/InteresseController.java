e77fa74477c4fde4217020f935f6432823c94f98
Paola Zanella <Paola Zanella@192.168.0.105>
2022-06-08 17:04:02 -0300

    cm-arruamndo interesse

--- src/main/java/br/com/desafio2/interesseservice/controller/InteresseController.java
+++ src/main/java/br/com/desafio2/interesseservice/controller/InteresseController.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package br.com.desafio2.interessesevice.controller;
+package br.com.desafio2.interesseservice.controller;
 
 import br.com.desafio2.interesseservice.model.Interesse;
 import br.com.desafio2.interesseservice.model.InteresseIdentity;
