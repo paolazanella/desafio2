42df2bae2a0cf6f56d5d4d075d6c3c58e939c3ec
Paola Zanella <Paola Zanella@192.168.0.105>
2022-06-10 17:33:46 -0300

    cm-arruamndo arumando tipo-imovel get/ alteracao feita no package Alpplication foi colocado a @EnableFeignClients

--- src/main/java/br/com/desafio2/tipoimovelservice/TipoImovelServiceApplication.java
+++ src/main/java/br/com/desafio2/tipoimovelservice/TipoImovelServiceApplication.java
@@ -2,8 +2,10 @@ package br.com.desafio2.tipoimovelservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class TipoImovelServiceApplication {
 
 	public static void main(String[] args) {
